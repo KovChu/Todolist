250ba74a5e815234da81d5682beef4204e5f936c
KovChu <kovchu@gmail.com>
2015-05-14 15:19:34 +0900

    revert main layout to include fragment in xml

--- README.md
+++ README.md
@@ -11,11 +11,8 @@ http://ormlite.com/sqlite_java_android_orm.shtml
 SQLite library for easy data structure and actions
 
 RecyclerView
-
 https://github.com/twotoasters/RecyclerViewLib
-
 for supporting RecyclerView for 4.0 and above
 
 This project is committed to GitHub, and can be view at
-
 https://github.com/KovChu/Todolist
--- app/src/main/java/com/demo/kuanyi/todolist/MainActivity.java
+++ app/src/main/java/com/demo/kuanyi/todolist/MainActivity.java
@@ -3,22 +3,13 @@ package com.demo.kuanyi.todolist;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 
-import com.demo.kuanyi.todolist.list.ToDoListFragment;
-
 
 public class MainActivity extends ActionBarActivity {
-
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getFragmentManager().
-                beginTransaction().
-                add(R.id.container,
-                        ToDoListFragment.newInstance(),
-                        "FRAGMENT_LIST").
-                commit();
         Utils.initDataHelper(this);
     }
 
