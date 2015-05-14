1e58fa418a35ff70e22925b23e8501eba4a1e825
KovChu <kovchu@gmail.com>
2015-05-14 10:55:59 +0900

    basic project structure and setup

---
+++ app/src/main/java/com/demo/kuanyi/todolist/list/ToDoListFragment.java
@@ -0,0 +1,55 @@
+package com.demo.kuanyi.todolist.list;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.demo.kuanyi.todolist.R;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class ToDoListFragment extends Fragment {
+
+    public ToDoListFragment() {
+    }
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_list, container, false);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_list, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_add) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
---
+++ app/src/main/java/com/demo/kuanyi/todolist/model/ListItemTable.java
@@ -0,0 +1,7 @@
+package com.demo.kuanyi.todolist.model;
+
+/**
+ * Created by kuanyi on 15/5/14.
+ */
+public class ListItemTable {
+}
